ca87f15d29e65193ac0b7bea8c0ba4391ae404c1
Saurabh <saurabh.munde@nonstopio.com>
2025-04-01 15:42:25 +0530

    just some testing ignore this commit

--- src/Login.js
+++ src/Login.js
@@ -7,6 +7,7 @@ const Login = ({ setLoggedIn }) => {
 
   const handleLogin = () => {
     registerUsername(username);
+    location.reload();
   };
 
   // Disable button if WebSocket is not connected
